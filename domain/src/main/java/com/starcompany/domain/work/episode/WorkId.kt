45d63bf122ee63549aa1fd5e809f23a5f5b78da6
yoshida0261 <yoshida261@gmail.com>
2018-06-04 04:51:42 +0900

    Initial commit

---
+++ domain/src/main/java/com/starcompany/domain/work/episode/WorkId.kt
@@ -0,0 +1,6 @@
+package com.starcompany.domain.work.episode
+
+import com.starcompany.common
+
+
+internal class WorkId(value: String) : Identifier<String>(value)
---
+++ modules/common/src/main/java/com/starcompany/common/Identifier.kt
@@ -0,0 +1,13 @@
+package com.starcompany.common
+
+abstract class Identifier<T>(val value: T){
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(other !is Identifier<*>) return false
+        return value == other.value
+    }
+
+    override fun hashCode(): Int{
+        return value?.hashCode() ?: 0
+    }
+}
